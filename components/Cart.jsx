de2da3e55edb8d9dbeed24632b0746a67bc61df6
BoltUltra <mayowa.u.sunusi@gmail.com>
2023-02-08 21:23:34 +0100

    ...

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import { useStateContext } from "@/context/StateContext";
+import { urlFor } from "@/lib/client";
+import Link from "next/link";
+import React, { useRef } from "react";
+import toast from "react-hot-toast";
+import {
+  AiOutlineMinus,
+  AiOutlinePlus,
+  AiOutlineLeft,
+  AiOutlineShopping,
+} from "react-icons/ai";
+import { TiDeleteOutline } from "react-icons/ti";
 
 const Cart = () => {
+  const cartRef = useRef();
+  const { totalPrice, totalQuantities, cartItems, setShowCart } =
+    useStateContext();
   return (
-    <div>Cart</div>
-  )
-}
+    <div className="cart-wrapper" ref={cartRef}>
+      <div className="cart-container">
+        <button
+          type="button"
+          className="cart-heading"
+          onClick={() => setShowCart(false)}
+        >
+          <AiOutlineLeft />
+          <span className="heading">Your Cart</span>
+          <span className="cart-num-items">({totalQuantities} items)</span>
+        </button>
+        {cartItems.length < 1 && (
+          <div className="empty-cart">
+            <AiOutlineShopping size={150} />
+            <h3>Your shopping list is empty</h3>
+            <Link href="/">
+              <button
+                type="button"
+                onClick={() => setShowCart(false)}
+                className="btn"
+              >
+                Continue Shopping
+              </button>
+            </Link>
+          </div>
+        )}
 
-export default Cart
+        <div className="product-container">
+          {cartItems.length >= 1 &&
+            cartItems.map((item) => (
+              <div className="product" key={item._id}>
+                <img
+                  src={urlFor(item?.image[0])}
+                  className="cart-product-image"
+                />
+              </div>
+            ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,17 +1,25 @@
+import { useStateContext } from "@/context/StateContext";
 import Link from "next/link";
 import React from "react";
 import { AiFillShopping } from "react-icons/ai";
+import { Cart } from "./";
 
 const Navbar = () => {
+  const { showCart, setShowCart, totalQuantities } = useStateContext();
   return (
     <div className="navbar-container">
       <p className="logo">
         <Link href="/">Re-Commerce</Link>
       </p>
-      <button type="button" className="cart-icon" onClick="">
+      <button
+        type="button"
+        className="cart-icon"
+        onClick={() => setShowCart(true)}
+      >
         <AiFillShopping />
-        <span className="cart-item-qty">1</span>
+        <span className="cart-item-qty">{totalQuantities}</span>
       </button>
+      {showCart && <Cart />}
     </div>
   );
 };
